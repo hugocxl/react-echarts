91f8a0fb2512b952bcb6fa7b8e89dc64085c5f2b
Hugo Corta <corta.hugo@gmail.com>
2021-01-02 23:09:13 +0100

    Build review

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,71 +1,56 @@
-'use strict'
+"use strict";
 
 // Dependencies
-const pJson = require('../package.json')
-const webpack = require('webpack')
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const pJson = require("../package.json");
+const webpack = require("webpack");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 // Paths setup
-const srcPath = path.resolve(__dirname, '../src')
-const libPath = path.resolve(__dirname, '../lib')
-const entryPoint = path.resolve(__dirname, '../src/index.js')
-
-// Env
-const env = process.env.NODE_ENV || 'development'
+const srcPath = path.resolve(__dirname, "../src");
+const libPath = path.resolve(__dirname, "../lib");
+const entryPoint = path.resolve(__dirname, "../src/index.js");
 
 module.exports = {
-  target: 'web',
+  target: "web",
   entry: entryPoint,
   output: {
     path: libPath,
-    filename: 'index.js',
+    filename: "index.js",
     library: pJson.name,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
+    libraryTarget: "umd",
+    umdNamedDefine: true,
   },
   resolve: {
-    modules: [srcPath, 'node_modules'],
-    extensions: ['.js', '.json', '.jsx'],
+    modules: [srcPath, "node_modules"],
+    extensions: [".js", ".json", ".jsx"],
     alias: {
-      constants$: path.resolve(__dirname, '../src/constants'),
-      HOC$: path.resolve(__dirname, '../src/HOC'),
-      core$: path.resolve(__dirname, '../src/core'),
-      utils$: path.resolve(__dirname, '../src/utils'),
-      components$: path.resolve(__dirname, '../src/components'),
-      themes$: path.resolve(__dirname, '../src/themes')
-    }
+      constants$: path.resolve(__dirname, "../src/constants"),
+      HOC$: path.resolve(__dirname, "../src/HOC"),
+      core$: path.resolve(__dirname, "../src/core"),
+      utils$: path.resolve(__dirname, "../src/utils"),
+      components$: path.resolve(__dirname, "../src/components"),
+      themes$: path.resolve(__dirname, "../src/themes"),
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(env)
-      },
-      NODE_ENV: env,
-      ENV_PRODUCTION: env === 'production',
-      ENV_DEVELOPMENT: env !== 'production'
-    }),
     new webpack.ProvidePlugin({
-      React: 'react',
-      PropTypes: 'prop-types'
-    })
+      React: "react",
+      PropTypes: "prop-types",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            cacheDirectory: false
-          }
-        }
+            cacheDirectory: false,
+          },
+        },
       },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
-      }
-    ]
-  }
-}
+    ],
+  },
+};
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,34 +1,40 @@
-'use strict'
+"use strict";
 
 // Dependencies
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const { merge } = require('webpack-merge')
-const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin')
+const { merge } = require("webpack-merge");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 // Common webpack config
-const common = require('./webpack.common.js')
+const common = require("./webpack.common.js");
 
 // Paths setup
-const srcPath = path.resolve(__dirname, '..', 'src')
+const srcPath = path.resolve(__dirname, "../src");
+const libPath = path.resolve(__dirname, "../lib");
+const readmePath = path.resolve(__dirname, "../README.md");
 
 const webpackconfig = {
-  mode: 'production',
+  mode: "production",
   plugins: [
+    new CopyPlugin({
+      patterns: [{ from: readmePath, to: libPath }],
+    }),
     new UglifyJsPlugin({
       include: [srcPath],
       parallel: true,
-      cache: true
-    })
+      cache: true,
+    }),
   ],
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({
-        parallel: true
-      })
-    ]
-  }
-}
+        parallel: true,
+      }),
+    ],
+  },
+};
 
-module.exports = merge(common, webpackconfig)
+module.exports = merge(common, webpackconfig);
