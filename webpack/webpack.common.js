5eb777fa6aca1acdbc07bd81f06283a0c16b5a4d
Hugo Corta <corta.hugo@gmail.com>
2021-01-02 23:11:29 +0100

    Build review

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,56 +1,56 @@
-"use strict";
+'use strict'
 
 // Dependencies
-const pJson = require("../package.json");
-const webpack = require("webpack");
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const pJson = require('../package.json')
+const webpack = require('webpack')
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 // Paths setup
-const srcPath = path.resolve(__dirname, "../src");
-const libPath = path.resolve(__dirname, "../lib");
-const entryPoint = path.resolve(__dirname, "../src/index.js");
+const srcPath = path.resolve(__dirname, '../src')
+const libPath = path.resolve(__dirname, '../lib')
+const entryPoint = path.resolve(__dirname, '../src/index.js')
 
 module.exports = {
-  target: "web",
+  target: 'web',
   entry: entryPoint,
   output: {
     path: libPath,
-    filename: "index.js",
+    filename: 'index.js',
     library: pJson.name,
-    libraryTarget: "umd",
-    umdNamedDefine: true,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   resolve: {
-    modules: [srcPath, "node_modules"],
-    extensions: [".js", ".json", ".jsx"],
+    modules: [srcPath, 'node_modules'],
+    extensions: ['.js', '.json', '.jsx'],
     alias: {
-      constants$: path.resolve(__dirname, "../src/constants"),
-      HOC$: path.resolve(__dirname, "../src/HOC"),
-      core$: path.resolve(__dirname, "../src/core"),
-      utils$: path.resolve(__dirname, "../src/utils"),
-      components$: path.resolve(__dirname, "../src/components"),
-      themes$: path.resolve(__dirname, "../src/themes"),
-    },
+      constants$: path.resolve(__dirname, '../src/constants'),
+      HOC$: path.resolve(__dirname, '../src/HOC'),
+      core$: path.resolve(__dirname, '../src/core'),
+      utils$: path.resolve(__dirname, '../src/utils'),
+      components$: path.resolve(__dirname, '../src/components'),
+      themes$: path.resolve(__dirname, '../src/themes')
+    }
   },
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.ProvidePlugin({
-      React: "react",
-      PropTypes: "prop-types",
-    }),
+      React: 'react',
+      PropTypes: 'prop-types'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            cacheDirectory: false,
-          },
-        },
-      },
-    ],
-  },
-};
+            cacheDirectory: false
+          }
+        }
+      }
+    ]
+  }
+}
