15d7bb6a15d55ac2a2fcf7d8ee40008ad033be5a
Hugo Corta <corta.hugo@gmail.com>
2021-01-11 12:03:54 +0100

    Added gragh and effectScatter

---
+++ src/components/EffectScatterChart.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import { Chart } from './Chart.js'
+import { useOption } from 'hooks'
+
+export function EffectScatterChart (props) {
+  const option = useOption({
+    ...props,
+    type: 'effectScatter'
+  })
+
+  return <Chart {...props} option={option} />
+}
