4ce5f587985482df8264831ca250e438a52311f1
Hugo Corta <corta.hugo@gmail.com>
2023-12-04 22:52:50 +0100

    Add nextra for docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
   output: 'export',
   basePath: '/react-echarts'
-}
+})
 
-module.exports = nextConfig
+module.exports = withNextra()
